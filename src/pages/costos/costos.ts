ff7f9c8e9d2349e0d5d2a732f386548e27ac9666
Elvis Velasque <elvisvelasque96@gmail.com>
2017-11-26 09:10:28 -0500

    gg

--- src/pages/costos/costos.ts
+++ src/pages/costos/costos.ts
@@ -1,13 +1,131 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { FormControl, FormBuilder } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Content } from 'ionic-angular';
 
 @Component({
   selector: 'costos-lists',
   templateUrl: 'costos.html'
 })
 export class costospage{
+toUser = {
+    _id: '534b8e5aaa5e7afc1b23e69b',
+    pic: 'assets/img/curioso.gif',
+    username: 'Pakita',
+  };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,) {}
+  user = {
+    _id: '534b8fb2aa5e7afc1b23e69c',
+    pic: 'assets/img/ian-avatar.png',
+    username: 'Usuario',
+  };
+
+  doneLoading = false;
+
+  messages = [
+    {
+      _id: 1,
+      date: new Date(),
+      userId: this.user._id,
+      username: this.user.username,
+      pic: this.user.pic,
+      text: 'OH CRAP!!'
+    },
+    {
+      _id: 2,
+      date: new Date(),
+      userId: this.toUser._id,
+      username: this.toUser.username,
+      pic: this.toUser.pic,
+      text: 'what??'
+    },
+    {
+      _id: 3,
+      date: new Date(),
+      userId: this.toUser._id,
+      username: this.toUser.username,
+      pic: this.toUser.pic,
+      text: 'Pretty long message with lots of content'
+    },
+    {
+      _id: 4,
+      date: new Date(),
+      userId: this.user._id,
+      username: this.user.username,
+      pic: this.user.pic,
+      text: 'Pretty long message with even way more of lots and lots of content'
+    },
+    {
+      _id: 5,
+      date: new Date(),
+      userId: this.user._id,
+      username: this.user.username,
+      pic: this.user.pic,
+      text: 'what??'
+    },
+    {
+      _id: 6,
+      date: new Date(),
+      userId: this.toUser._id,
+      username: this.toUser.username,
+      pic: this.toUser.pic,
+      text: 'yes!'
+    }
+  ];
+
+  @ViewChild(Content) content: Content;
+
+  public messageForm: any;
+  chatBox: any;
+
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+    this.messageForm = formBuilder.group({
+      message: new FormControl('')
+    });
+    this.chatBox = '';
+
+  }
+
+  send(message) {
+    if (message && message !== '') {
+      // this.messageService.sendMessage(chatId, message);
+
+      const messageData =
+        {
+          toId: this.toUser._id,
+          _id: 6,
+          date: new Date(),
+          userId: this.user._id,
+          username: this.toUser.username,
+          pic: this.toUser.pic,
+          text: message
+        };
+
+      this.messages.push(messageData);
+      this.scrollToBottom();
+
+      setTimeout(() => {
+        const replyData =
+          {
+            toId: this.toUser._id,
+            _id: 6,
+            date: new Date(),
+            userId: this.toUser._id,
+            username: this.toUser.username,
+            pic: this.toUser.pic,
+            text: 'Just a quick reply'
+          };
+        this.messages.push(replyData);
+        this.scrollToBottom();
+      }, 1000);
+    }
+    this.chatBox = '';
+  }
+
+  scrollToBottom() {
+    setTimeout(() => {
+      this.content.scrollToBottom();
+    }, 100);
+  }
 
 
 }
