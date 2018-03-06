2923e2ef3f8ddd1962512d77c1ec8d7892400350
Elvis Velasque <elvisvelasque96@gmail.com>
2018-03-05 23:10:39 -0500

    carga 60%

---
+++ src/app/app.module.ts
@@ -0,0 +1,90 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ventaspage } from '../pages/ventas/ventas';
+import { cargarpage } from '../pages/cargar/cargar';
+import { compraspage } from '../pages/compras/compras';
+import { proveedorespage } from '../pages/proveedores/proveedores';
+import { metaspage } from '../pages/metas/metas';
+import { metasAdd } from '../pages/metas/metas-add/metas-add';
+import { resumenpage } from '../pages/resumen/resumen';
+import { CameraProvider } from '../providers/camera.provider';
+import { InvoiceProvider } from '../providers/InvoiceProvider';
+import { LoginSliderPage } from '../pages/login-slider/login-slider';
+import { ventasPeriodo } from '../pages/ventas/ventasPeriodo/ventasPeriodo';
+import { ventaProyecciones } from '../pages/ventas/ventaProyecciones/ventaProyecciones';
+import { ventasEstrategias } from '../pages/ventas/ventasEstrategias/ventasEstrategias';
+import { comprasPeriodo } from '../pages/compras/comprasPeriodo/comprasPeriodo';
+import { comprasProyecciones } from '../pages/compras/comprasProyecciones/comprasProyecciones';
+import { comprasEstrategias } from '../pages/compras/comprasEstrategias/comprasEstrategias';
+import { estrategias } from '../pages/estrategias/estrategias';
+import { estratpreciopreg } from '../pages/estrategias/estratpreciopreg/estratpreciopreg';
+import { clientespage } from '../pages/clientes/clientes';
+import { marketing } from '../pages/marketing/marketing';
+import { chatpakitapage } from '../pages/chatPakita/chatPakita';
+
+import { FileChooser } from '@ionic-native/file-chooser';
+import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ventaspage,
+    cargarpage,
+   // inventariopage,
+    compraspage,
+    metaspage,
+    metasAdd,
+    proveedorespage,
+    resumenpage,
+    LoginSliderPage,
+    ventasPeriodo,
+    ventaProyecciones,
+    ventasEstrategias,
+    comprasPeriodo,
+    comprasProyecciones,
+    comprasEstrategias,
+    estrategias,
+    estratpreciopreg,
+    clientespage,
+    chatpakitapage,
+    marketing  
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ventaspage,
+    cargarpage,
+    proveedorespage,
+    compraspage,
+    metaspage,
+    metasAdd,
+    resumenpage,
+    LoginSliderPage,
+    ventasPeriodo,
+    ventaProyecciones,
+    ventasEstrategias,
+    comprasPeriodo,
+    comprasProyecciones,
+    comprasEstrategias,
+    estrategias,
+    estratpreciopreg,
+    clientespage,
+    chatpakitapage,
+    marketing
+  ],
+  providers: [
+  FileTransfer,
+  FileTransferObject,
+  File,
+  FileChooser,
+  CameraProvider,InvoiceProvider, {provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
--- src/pages/cargar/cargar.ts
+++ src/pages/cargar/cargar.ts
@@ -1,7 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular'
+import { NavController, NavParams, Platform,LoadingController, ToastController  } from 'ionic-angular'
 import { FileChooser } from '@ionic-native/file-chooser';
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
 
 
 
@@ -16,6 +17,8 @@ export class cargarpage{
 
 
   public backgroundImage = 'assets/img/nube3.png';
+  imageFileName:any;
+  imageURI:any;
 
   constructor(
     public platform: Platform,
@@ -23,6 +26,8 @@ export class cargarpage{
     public navParams: NavParams,
     private fileChooser: FileChooser,
     private transfer: FileTransfer,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController
 
     )
   {
@@ -30,7 +35,45 @@ export class cargarpage{
 
   ngAfterViewInit() { }
 
-  uploadresume()
+ 
+
+  uploadFile() {
+
+  this.fileChooser.open()
+      .then(uri => 
+      {
+        console.log(uri)
+         const fileTransfer: FileTransferObject = this.transfer.create();
+       });
+      
+  let loader = this.loadingCtrl.create({
+    content: "Uploading..."
+  });
+  loader.present();
+  const fileTransfer: FileTransferObject = this.transfer.create();
+
+  let options: FileUploadOptions = {
+    fileKey: 'ionicfile',
+    fileName: 'ionicfile',
+    chunkedMode: false,
+    mimeType: "file/xml",
+    headers: {}
+  }
+
+  fileTransfer.upload(this.imageURI, 'http://192.168.0.7:8080/api/uploadImage', options)
+    .then((data) => {
+    console.log(data+" Uploaded Successfully");
+    this.imageFileName = "http://192.168.0.7:8080/static/images/ionicfile.jpg"
+    loader.dismiss();
+  }, (err) => {
+    console.log(err);
+    loader.dismiss();
+  });
+
+
+
+
+  /*uploadresume()
   {
       this.fileChooser.open()
       .then(uri => 
@@ -60,6 +103,8 @@ export class cargarpage{
 
       })
       .catch(e => console.log(e));
-  }
+  } */
+
+}
 
 }
